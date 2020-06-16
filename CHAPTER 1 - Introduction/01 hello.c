469ed0c9fe8e21979693595d9a14a3f7f6c03fb2
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2020-06-16 03:24:57 +0300

    Hello World

---
+++ CHAPTER 1 - Introduction/01 hello.c
@@ -0,0 +1,13 @@
+// including standard input output library
+#include <stdio.h>
+
+main()
+{
+	// \n stands for new line in this character string
+	printf("Hello, world\n");
+	
+	// you can get same results by dividing string into different functions
+	printf("Hello,");
+	printf(" world");
+	printf("\n");
+}
