8862eb6276fcee07c869721a55c525376d1d9bb0
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2020-06-16 03:27:08 +0300

    Fahrenheit - Celcius conversion table
    celcius = (5/9) * (fahr - 32)

---
+++ CHAPTER 1 - Introduction/03 fahrenheit v2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	/* print Fahrenheit - Celcius table */
+	
+	float fahr, celcius;
+	float lower, upper, step;
+	
+	lower = 0;
+	upper = 300;
+	step = 20;
+	
+	printf(" fahr   celcius\n");
+	fahr = lower;
+	while (fahr <= upper) {
+		celcius = (5.0/9.0) * (fahr - 32.0);
+		printf("%4.0f\t%6.2f\n", fahr, celcius);
+		fahr = fahr + step;
+	}
+}
